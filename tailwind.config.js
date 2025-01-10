2c844903e85d4e3599c0df75b1f8dde9d5bd88ac
Ath1ete <biplabtripathi3@gmail.com>
2025-01-10 17:15:13 +0530

    Configured the content sources for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/components/*.{tsx}",
+    "./src/features/**/*.{tsx}",
+    "./src/layouts/*.{tsx}",
+    "./src/pages/**/*.{tsx}",
+    "./src/*.{tsx}",
+  ],
   theme: {
     extend: {},
     fontFamily: {
@@ -9,3 +15,10 @@ export default {
   },
   plugins: [],
 };
+
+// The path pattern used in the content array. Here's a simpler breakdown:
+
+// ./src/: This refers to the src folder in your project directory.
+// " **/ ": This means "look in all subfolders inside the src folder, no matter how many levels deep."
+// " *.{html, js, tsx, jsx} ": This means "include all files that end with .html, .js, .tsx, .jsx"
+// In essence, Tailwind uses this to find all the classes you're using in these files and generate only the necessary CSS for your project.
